71711b93286f9a2b3bbc4e5ec7c1d68683beb324
Мамедов Дмитрий <mamedov@vbankcenter.ru>
2022-10-26 22:55:35 +0300

    Added. signUp validation

--- src/main/java/edu/school21/springboot/config/WebAppConfig.java
+++ src/main/java/edu/school21/springboot/config/WebAppConfig.java
@@ -3,11 +3,64 @@ package edu.school21.springboot.config;
 import no.api.freemarker.java8.Java8ObjectWrapper;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
+import java.util.Locale;
+
 @Configuration
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver clr = new CookieLocaleResolver();
+		clr.setDefaultLocale(Locale.US);
+		clr.setCookieName("LOCALE");
+		return clr;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean getValidator() {
+		LocalValidatorFactoryBean factoryBean = new LocalValidatorFactoryBean();
+		factoryBean.setValidationMessageSource(messageSource());
+		return factoryBean;
+	}
+
+	@Bean
+	public MessageCodesResolver messageCodesResolver() {
+		return new DefaultMessageCodesResolver();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
 
 	@Configuration
 	public static class FreeMarkerObjectWrapperConfig implements BeanPostProcessor {
--- src/main/java/edu/school21/springboot/dto/SignUpInDto.java
+++ src/main/java/edu/school21/springboot/dto/SignUpInDto.java
@@ -1,19 +1,31 @@
 package edu.school21.springboot.dto;
 
+import lombok.Builder;
 import lombok.Value;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Builder
 @Value
 public class SignUpInDto {
 
 	/** Фамилия пользователя */
+	@Size(message = "{error.validation.signUp.lastName}", min = 1, max = 127)
 	String lastName;
 	/** Имя пользователя */
+	@Size(message = "{error.validation.signUp.firstName}", min = 1, max = 127)
 	String firstName;
 	/** Телефон пользователя */
+	@Pattern(regexp = "^\\+7\\(\\d\\d\\d\\)\\d\\d\\d\\d\\d\\d\\d", message = "{error.validation.signUp.phone}")
 	String phone;
 	/** Почта пользователя */
+	@Size(message = "{error.validation.signUp.email}", min = 6, max = 320)
+	@Email(message = "{error.validation.signUp.email}")
 	String email;
 	/** Пароль */
+	@Size(message = "{error.validation.signUp.password}", min = 5, max = 255)
 	String password;
 
 }
